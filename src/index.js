306c2d07b80b70c6d5557e5f440415589764a20b
CARLOS AGUIRRE <carlos.agu@gmail.com>
2017-08-31 17:59:52 -0600

    Hasta la seleccion del video

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import ReacDOM from 'react-dom';
 import React, { Component } from 'react';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 import YTSearch from 'youtube-api-search'
 
 const API_KEY = 'AIzaSyBuQGaeyGjuEXySwtuLY8d6EKpUpfPrpGQ';
@@ -9,16 +11,26 @@ const API_KEY = 'AIzaSyBuQGaeyGjuEXySwtuLY8d6EKpUpfPrpGQ';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { videos: [] }
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        }
         YTSearch({ key: API_KEY, term: 'Rihanna' }, videos => {
-            this.setState({ videos });
+            this.setState(
+                { videos: videos,
+                selectedVideo: videos[0] 
+            });
         });
     }
     render() {
         return (
             <div>
                 <SearchBar />
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}
+                />
             </div>
         );
     }

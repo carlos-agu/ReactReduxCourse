2fd73d2d4743bfec5d49ad37e7a4835e07846ba6
Carlos Aguirre <carlos.agu@gmail.com>
2017-08-31 14:07:46 -0600

    Commit para enviar a GitHub

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { Component } from 'react';
 
 class SearchBar extends Component{
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            term: ''
+        }
+    }
     
     render(){
         return (
-            <input type="text"/>
+            <div>
+                <input 
+                    value={this.state.term}
+                    onChange={evt => this.setState({term: evt.target.value})}/>
+                <br/>                
+            </div>
         );
     }
+    
 }
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import ReacDOM from 'react-dom';
-import React from 'react';
+import React, { Component } from 'react';
 import SearchBar from './components/search_bar';
-
+import YTSearch from 'youtube-api-search'
 const API_KEY = 'AIzaSyBuQGaeyGjuEXySwtuLY8d6EKpUpfPrpGQ';
 
-const App = () =>{
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] }
+        YTSearch({ key: API_KEY, term: 'Rihanna' }, videos => {
+            this.setState({ videos });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 ReacDOM.render(<App />, document.querySelector('.container'));

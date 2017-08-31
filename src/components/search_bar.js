fe010260b6c0465ee904a6fa4c9408025fd02c52
Carlos Aguirre <carlos.agu@gmail.com>
2017-08-31 10:33:51 -0600

    Commit de Prueba

---
+++ src/components/search_bar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+class SearchBar extends Component{
+    
+    render(){
+        return (
+            <input type="text"/>
+        );
+    }
+}
+export default SearchBar;
